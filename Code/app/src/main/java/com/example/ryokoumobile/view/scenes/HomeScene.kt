c2a28594a9a3ba45d86bc3c965edde0425cb5682
HyuNie <huunien734@gmail.com>
2024-12-27 15:35:57 +0700

    + Add ItemTour

--- Code/app/src/main/java/com/example/ryokoumobile/view/scenes/HomeScene.kt
+++ Code/app/src/main/java/com/example/ryokoumobile/view/scenes/HomeScene.kt
@@ -2,27 +2,56 @@ package com.example.ryokoumobile.view.scenes
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ryokoumobile.model.controller.DataController
+import com.example.ryokoumobile.model.repository.lsPager
 import com.example.ryokoumobile.ui.theme.RyokouMobileTheme
 import com.example.ryokoumobile.view.components.FlashSaleTimeCountDown
 import com.example.ryokoumobile.view.components.MyHorizontalPage
 import com.example.ryokoumobile.view.components.MyTopBar
 import com.example.ryokoumobile.view.components.RangeTimeScale
+import com.example.ryokoumobile.view.items.ItemTour
+import com.example.ryokoumobile.viewmodel.HomeViewModel
+import kotlinx.coroutines.delay
+import java.time.LocalTime
 
 @Composable
-fun HomeScene() {
-    Scaffold(topBar = { MyTopBar() }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding).padding(vertical = 10.dp)) {
-            MyHorizontalPage(listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj0eiBXaznxdQOnmOYzhH67_T6HeNbGQMDwA&s",
-                "https://static-oku.diqit.io/assets/worldwide/oku_ways_to_go_images4.jpg","abc","dcf","aaa"))
-            Column(modifier = Modifier.padding(horizontal = 10.dp)) {
-                RangeTimeScale()
-                FlashSaleTimeCountDown()
+fun HomeScene(viewModel: HomeViewModel = viewModel()) {
+    val uiState = viewModel.uiState.collectAsState()
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(1000)
+            viewModel.updateTime(LocalTime.now())
+            viewModel.updateDuration()
+        }
+    }
 
+    Scaffold(topBar = { MyTopBar() }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(vertical = 10.dp)
+        ) {
+            MyHorizontalPage(
+                lsLinkPicture = lsPager
+            )
+            Column(modifier = Modifier.padding(horizontal = 10.dp)) {
+                RangeTimeScale(uiState.value.startTime)
+                FlashSaleTimeCountDown(uiState.value.duration)
+                LazyRow {
+                    items(DataController.lsTour) { tour ->
+                        ItemTour(tour, {})
+                    }
+                }
             }
         }
     }
