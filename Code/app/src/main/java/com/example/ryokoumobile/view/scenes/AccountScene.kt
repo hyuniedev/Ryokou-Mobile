777d88dfc6f1f47861aa89e335854b1994dd3c25
HyuNie <huunien734@gmail.com>
2025-01-12 03:31:02 +0700

    + add button log out temporary

--- Code/app/src/main/java/com/example/ryokoumobile/view/scenes/AccountScene.kt
+++ Code/app/src/main/java/com/example/ryokoumobile/view/scenes/AccountScene.kt
@@ -1,6 +1,7 @@
 package com.example.ryokoumobile.view.scenes
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,10 +10,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.ryokoumobile.model.controller.FirebaseController
+import com.example.ryokoumobile.view.components.MyElevatedButton
 
 @Composable
 fun AccountScene(modifier: Modifier = Modifier, navController: NavController) {
-    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Column(modifier.fillMaxSize()) {
         Text("Account Scene", style = TextStyle(fontSize = 30.sp))
+        MyElevatedButton(title = "Log out") {
+            FirebaseController.SignOut()
+        }
     }
 }
