0ebc553d539f94733e6eea67b74464bdc2c2cfa0
HyuNie <huunien734@gmail.com>
2025-01-29 08:55:36 +0700

    + base view my tour screen

--- Code/app/src/main/java/com/example/ryokoumobile/view/scenes/MyTourScene.kt
+++ Code/app/src/main/java/com/example/ryokoumobile/view/scenes/MyTourScene.kt
@@ -1,7 +1,9 @@
 package com.example.ryokoumobile.view.scenes
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,11 +15,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,15 +45,17 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.ryokoumobile.R
 import com.example.ryokoumobile.model.controller.DataController
+import com.example.ryokoumobile.model.entity.TourBooked
 import com.example.ryokoumobile.model.repository.Scenes
 import com.example.ryokoumobile.view.components.MyElevatedButton
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Composable
 fun MyTourScene(modifier: Modifier = Modifier, navController: NavController) {
     val user = DataController.user.collectAsState()
-    Box(modifier = modifier) {
+    Box(modifier = modifier.padding(vertical = 15.dp)) {
         when (user.value) {
-//            null -> OnNotLoggedIn(navController)
+            null -> OnNotLoggedIn(navController)
 
             else -> OnLoggedIn()
         }
@@ -84,7 +98,7 @@ private fun OnNotLoggedIn(navController: NavController) {
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-                .fillMaxWidth(0.75f),
+                .fillMaxWidth(0.8f),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -109,5 +123,77 @@ private fun OnNotLoggedIn(navController: NavController) {
 
 @Composable
 private fun OnLoggedIn() {
-    
+    val indexSelected = remember { MutableStateFlow(0) }
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(scrollState)) {
+        SectionTour(
+            title = "Tour đang diễn ra",
+            index = 0,
+            indexSelected = indexSelected.collectAsState().value,
+            lsTour = listOf()
+        ) { index -> indexSelected.value = index }
+        SectionTour(
+            title = "Tour sắp diễn ra",
+            index = 1,
+            indexSelected = indexSelected.collectAsState().value,
+            lsTour = listOf()
+        ) { index -> indexSelected.value = index }
+        SectionTour(
+            title = "Tour đã hoàn thành",
+            index = 2,
+            indexSelected = indexSelected.collectAsState().value,
+            lsTour = listOf()
+        ) { index -> indexSelected.value = index }
+    }
+}
+
+@Composable
+private fun SectionTour(
+    title: String,
+    index: Int,
+    indexSelected: Int,
+    lsTour: List<TourBooked>,
+    onClick: (Int) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize()
+            .padding(horizontal = 10.dp),
+        onClick = { onClick(index) }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp, vertical = 7.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                title,
+                style = TextStyle(
+                    fontSize = 21.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            )
+            Icon(
+                if (index == indexSelected) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null,
+                modifier = Modifier.size(31.dp)
+            )
+        }
+        when (index) {
+            0 -> {
+                // TODO: Thêm ItemTourBooked và View cho Tour đang diễn ra
+            }
+
+            else -> {
+                // TODO: Thực hiện hiển thị list các tourBooked
+            }
+        }
+    }
 }
